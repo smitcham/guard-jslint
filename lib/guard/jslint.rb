a7c50664aab566bb15d19b19ed23cb15fd173ba0
Frank Wöckener <fw@cubiflex.com>
2010-12-14 09:02:29 +0100

    iterate through paths

--- lib/guard/jslint.rb
+++ lib/guard/jslint.rb
@@ -4,12 +4,10 @@ require 'guard/guard'
 module Guard
   class Jslint < Guard
     
-    def initialize(watchers = [], options = {})
-      super
-    end
-    
     def run_on_change(paths)
-      system("node app/javascripts/plugins/jslint.js #{paths}")
+      paths.each do |path|
+        system("node app/javascripts/plugins/jslint.js #{path}")
+      end
     end
     
   end
