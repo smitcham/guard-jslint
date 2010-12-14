6c71d57009e42b4ca38e586b8cdfe3a5456aaaad
Frank Wöckener <fw@cubiflex.com>
2010-12-14 09:30:47 +0100

    removed unnecessary params

--- lib/guard/jslint.rb
+++ lib/guard/jslint.rb
@@ -5,6 +5,7 @@ module Guard
   class Jslint < Guard
     
     def run_on_change(paths)
+      system("clear")
       paths.each do |path|
         puts "Running jslint for #{path}:"
         system("node app/javascripts/plugins/jslint.js #{path}")
